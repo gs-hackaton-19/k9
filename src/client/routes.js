82b48e575bfb2cd73991aa2ecb43f462a684c325
Milan Sarosac <milan.sarosac@jaystack.com>
2019-09-27 11:13:04 +0200

    clear up things

--- src/client/routes.js
+++ src/client/routes.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import { Switch } from 'react-router'
-import App from './App'
 import RouteWithLayout from './RouteWithLayout'
 import Dashboard from './containers/Dashboard/Dashboard'
 import TakeHomeRequestList from './containers/Admin/TakeHomeRequestList';
 
 export default () => (
-  <App>
-    <Switch>
-      <RouteWithLayout path="/admin" component={TakeHomeRequestList} />
-      <RouteWithLayout path="/" component={Dashboard} />
-    </Switch>
-  </App>
+  <Switch>
+    <RouteWithLayout path="/admin" component={TakeHomeRequestList} />
+    <RouteWithLayout path="/dashboard" component={Dashboard} />
+  </Switch>
 )
