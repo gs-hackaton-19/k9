ec44c580f16b2a0b6d8a8e421ed61e2399b2e0d8
Kadosa Kocsis <kadosa.kocsis@jaystack.com>
2019-09-27 12:21:56 +0200

    Modified upload.js

--- testData/upload.js
+++ testData/upload.js
@@ -1,5 +1,5 @@
 const axios = require('axios')
 const pets = require('./pet.json')
 pets.forEach( (pet) => {
-    axios.post('http://localhost:3001/api/pet', pet)..then("Success").catch(console.log)    
+    axios.post('http://localhost:3001/api/pet', pet).then("Success").catch(console.log)    
 });    
