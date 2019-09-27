d5979cb7591b6c4df3137146330e5c1b10042d6e
László Bernát <bernatl@jaystack.com>
2019-09-27 15:38:06 +0200

    CUcc

--- src/client/containers/Dashboard/MapTest.jsx
+++ src/client/containers/Dashboard/MapTest.jsx
@@ -4,28 +4,42 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import compose from 'recompose/compose'
 import { withStyles } from '@material-ui/core/styles'
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+
+const AnyReactComponent = ({ text }) => <div class='google.maps.Marker'>{text}</div>;
 
 const styles = theme => ({
 
 });
+
 export class MapTest extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: -25.363,
+      lng: 131.044,
+      marker_text: "Kuty"
+    }
   }
 
   render() {
-
     return (
-      <div>
-        Google Maps
-      </div>
+      <Map
+          google={this.props.google}
+          zoom={8}
+          style={mapStyles}
+          initialCenter={{ lat: this.state.lat, lng: this.state.lng}}
+        >
+          <Marker position={{ lat: this.state.lat, lng: this.state.lng}} />
+        </Map>
     )
   }
 }
 
 function mapStateToProps(state) {
   return {
-
+    apiKey: 'AIzaSyAfxDsmfHbRdZexPRLm_wJbwme1rQ23-vo'
   }
 }
 
@@ -37,5 +51,10 @@ function mapDispatchToProps(dispatch) {
 export default compose(
   withStyles(styles),
   connect(mapStateToProps, mapDispatchToProps),
+  GoogleApiWrapper(mapStateToProps)
 )(MapTest)
 
+const mapStyles = {
+  width: '100%',
+  height: '100%',
+};
